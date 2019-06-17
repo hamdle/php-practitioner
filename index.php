a37e502edf103bd8ec26b1d494fbff6c45dbdc14
hamdle <contact@ericmarty.co>
2019-06-17 15:05:15 -0700

    Separate PHP Logic From Presentation

--- index.php
+++ index.php
@@ -1,24 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <style>
-        header {
-            background: gray;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-    <header>
-        <h1>
-            <?php       
-                // URL: http://localhost:8000/?name=%3Ca%20href=%22https://ericmarty.dev%22%3EEric%3C/a%3E          
-                echo "Hello, " . htmlspecialchars($_GET['name']);
-            ?>
-        </h1>
-    </header>
-</body>
-</html>
+<?php
+
+$greeting = "Eric Marty";
+
+require "index.view.php";
---
+++ index.view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
+        header {
+            background: gray;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>
+            <?php echo $greeting ?>
+        </h1>
+    </header>
+</body>
+</html>
